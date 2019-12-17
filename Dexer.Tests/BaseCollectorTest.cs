3f280c5b581cb463b47d242b7304e9a341abd459
Sebastien Lebreton <sebastien@lebreton.fr>
2019-12-17 10:49:09 -0800

    Refactor for nuget publishing

---
+++ Dexer.Tests/BaseCollectorTest.cs
@@ -0,0 +1,62 @@
+﻿/* Dexer Copyright (c) 2010-2019 Sebastien LEBRETON
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dexer.Core;
+using Dexer.IO.Collectors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dexer.Tests
+{
+	[TestClass]
+	public class BaseCollectorTest : BaseTest
+	{
+		internal void TestCollector<TC, T>(Func<Dex, List<T>> provider) where TC : BaseCollector<T>, new()
+		{
+			foreach (var file in GetTestFiles())
+			{
+				TestCollector<TC, T>(provider, file);
+			}
+		}
+
+		internal TC TestCollector<TC, T>(Func<Dex, List<T>> provider, string file) where TC : BaseCollector<T>, new()
+		{
+			TestContext.WriteLine("Testing {0}", file);
+			var dex = Dex.Read(file);
+
+			var collector = new TC();
+			collector.Collect(dex);
+
+			var sourceKeys = provider(dex).ToDictionary(s => s);
+			var destKeys = collector.Items.ToDictionary(kv => kv.Key);
+
+			foreach (var key in sourceKeys.Keys)
+				Assert.IsTrue(collector.Items.ContainsKey(key) || (key.ToString() == "this"), "Item '{0}' not collected", key);
+
+			foreach (var key in destKeys.Keys)
+				Assert.IsTrue(sourceKeys.ContainsKey(key) || (key.ToString() == "this"), "Item '{0}' is 'over' collected", key);
+
+			return collector;
+		}
+	}
+}
---
+++ Dexer.Tests/RoundtripTest.cs
@@ -0,0 +1,115 @@
+﻿/* Dexer Copyright (c) 2010-2019 Sebastien LEBRETON
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Dexer.Core;
+using Dexer.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dexer.IO;
+using Dexer.Metadata;
+
+namespace Dexer.Tests
+{
+	[TestClass]
+	public class RoundtripTest : BaseTest
+	{
+		private void TestReadWrite(string file, out DexReader dexreader, out DexWriter dexwriter)
+		{
+			TestContext.WriteLine("Testing {0}", file);
+
+			var dex = new Dex();
+			dexreader = new DexReader(dex);
+
+			using (Stream fs = new FileStream(file, FileMode.Open))
+			using (var reader = new BinaryReader(fs))
+				dexreader.ReadFrom(reader);
+
+			dexwriter = new DexWriter(dex);
+
+			if (Extralog)
+			{
+				using (Stream fs = new FileStream(file + ".out", FileMode.Create))
+				using (var writer = new BinaryWriter(fs))
+					dexwriter.WriteTo(writer);
+			}
+			else
+			{
+				using (Stream fs = new MemoryStream())
+				using (var writer = new BinaryWriter(fs))
+					dexwriter.WriteTo(writer);
+			}
+		}
+
+		[TestMethod]
+		public void TestMap()
+		{
+			foreach (var file in GetTestFiles())
+			{
+				TestReadWrite(file, out var dexreader, out var dexwriter);
+
+				var checklist = new Dictionary<TypeCodes, string>();
+
+				foreach (var tc in dexwriter.Map.Keys.Where(tc => dexreader.Map.ContainsKey(tc)))
+				{
+					if (dexreader.Map[tc].Count != dexwriter.Map[tc].Count)
+					{
+						TestContext.WriteLine("{0} Count differs expected={1}, actual={2}", tc, dexreader.Map[tc].Count, dexwriter.Map[tc].Count);
+						if (!checklist.ContainsKey(tc))
+							checklist.Add(tc, tc.ToString());
+					}
+
+					if (dexreader.Map[tc].Offset != dexwriter.Map[tc].Offset)
+					{
+						TestContext.WriteLine("{0} Offset differs : expected={1}, actual={2}", tc, dexreader.Map[tc].Offset, dexwriter.Map[tc].Offset);
+						if (!checklist.ContainsKey(tc))
+							checklist.Add(tc, tc.ToString());
+					}
+				}
+
+				Assert.IsTrue(checklist.Count == 0, string.Concat("Check test report : ", string.Join(", ", checklist.Values)));
+			}
+		}
+
+		[TestMethod]
+		public void TestCheckSum()
+		{
+			foreach (var file in GetTestFiles())
+			{
+				TestReadWrite(file, out var dexreader, out var dexwriter);
+
+				Assert.AreEqual(dexreader.Header.CheckSum, dexwriter.CheckSum);
+			}
+		}
+
+		[TestMethod]
+		public void TestSignature()
+		{
+			foreach (var file in GetTestFiles())
+			{
+				TestReadWrite(file, out var dexreader, out var dexwriter);
+
+				Assert.IsTrue(dexreader.Header.Signature.Match(dexwriter.Signature, 0));
+			}
+		}
+	}
+}
