e7ec340b246de68fb83801957323fe2b20416cb0
Sebastien Lebreton <sebastien@lebreton.fr>
2019-12-17 08:29:46 -0800

    Bump version

--- Dexer/Properties/AssemblyInfo.cs
+++ Dexer/Properties/AssemblyInfo.cs
@@ -4,19 +4,11 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Dexer")]
 [assembly: AssemblyDescription("Dexer manipulation library for Android/Dalvik DEX files")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dexer")]
 [assembly: AssemblyCopyright("Copyright © 2010-2019 Sebastien LEBRETON")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
-
-[assembly: Guid("8271aa20-84e7-4ab6-b483-345320e722b2")]
-
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
 
 [assembly: InternalsVisibleTo("DexerDebug")]
 [assembly: InternalsVisibleTo("DexerTest")]
--- DexerTest/Properties/AssemblyInfo.cs
+++ DexerTest/Properties/AssemblyInfo.cs
@@ -3,16 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DexerDebug")]
 [assembly: AssemblyDescription("Dexer Test suite")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dexer")]
 [assembly: AssemblyCopyright("Copyright © 2010-2019 Sebastien LEBRETON")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
-
-[assembly: Guid("d4041b1f-2010-4dd8-90bc-cbe78ce71a8e")]
-
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
