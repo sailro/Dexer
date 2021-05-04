04cbae00b1e53a6dc2425f23d52dc31903f3e83c
Sebastien Lebreton <sebastien@lebreton.fr>
2021-05-03 21:01:50 -0700

    Update Copyright

--- Dexer/IO/Collectors/CatchSetComparer.cs
+++ Dexer/IO/Collectors/CatchSetComparer.cs
@@ -1,4 +1,4 @@
-﻿/* Dexer Copyright (c) 2010-2019 Sebastien LEBRETON
+﻿/* Dexer Copyright (c) 2010-2021 Sebastien Lebreton
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
